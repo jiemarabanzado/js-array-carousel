dca8c69342b05f0365be3be9044ea63e34174c9c
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-26 16:35:50 +0200

    next infinito

--- main.js
+++ main.js
@@ -4,5 +4,18 @@ for (let index = 0; index < images.length; index++) {
     a.innerHTML +=`<div class="item"><img src="${images[index]}" alt="img1"></div>`;             
 }           
 document.querySelector('.item').classList.add('active')
+let active=0; 
 
-    
+document.querySelector('.next').addEventListener("click" , function(){
+     let old=document.querySelectorAll('.item');
+     console.log(old);
+
+     if(active==old.length-1){
+        old[active].classList.remove('active');
+        active=0;
+        old[active].classList.add('active');}else{
+            old[active].classList.remove('active');
+            active= active+1;
+            old[active].classList.add('active');
+        }
+    });
