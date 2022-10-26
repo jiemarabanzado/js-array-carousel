304a2573bc1d9ee3276f6154079258386b7cf980
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-26 15:50:46 +0200

    creazione array immagini

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert('test collegamento')
+const images=["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg"];
+let a=document.querySelector('.items');
+for (let index = 0; index < images.length; index++) {
+    
+    a.innerHTML +=`<div class="item"><img src="${images[index]}" alt="img1"></div>`
+                
+}           
+
+    
