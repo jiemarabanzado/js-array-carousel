5a2791e136d932a1fd72bb564964f35ffc408273
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-26 15:53:46 +0200

    aggiunto active

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const images=["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg"];
 let a=document.querySelector('.items');
 for (let index = 0; index < images.length; index++) {
-    
-    a.innerHTML +=`<div class="item"><img src="${images[index]}" alt="img1"></div>`
-                
+    a.innerHTML +=`<div class="item"><img src="${images[index]}" alt="img1"></div>`;             
 }           
+document.querySelector('.item').classList.add('active')
 
     
